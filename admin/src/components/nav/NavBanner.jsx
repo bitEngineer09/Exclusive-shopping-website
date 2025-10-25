d86fd05730a04108f1e88f0ea265369125991a51
bitengineer09 <bitengineer09@gmail.com>
2025-10-25 19:04:11 +0530

    ..

--- admin/src/components/nav/NavBanner.jsx
+++ admin/src/components/nav/NavBanner.jsx
@@ -1,23 +1,40 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const NavBanner = () => {
+    const location = useLocation();
+    const currentPath = location.pathname;
+
     return (
-        <div
-            className='
-                    flex items-center justify-center text-center
-                    w-full h-auto md:h-15
-                    md:text-2xl
-                    bg-white
-                    py-2 md:py-0
-                    text-black font-semibold tracking-wider
-                '>
-            <p className='hidden xl:block '>
-                “Drive your business forward — the dashboard is your cockpit.”
-            </p>
-            <p className='block xl:hidden'>
-                "Hey there, boss! Let’s make today’s sales shine."
-            </p>
-        </div>
+        <>
+
+            {
+                currentPath == "/login" ?
+                    ""
+                    :
+                    (
+                        <div
+                            className='
+                                flex items-center justify-center text-center
+                                w-full h-auto md:h-15
+                                md:text-2xl
+                                bg-white
+                                py-2 md:py-0
+                                text-black font-semibold tracking-wider
+                                '>
+                            <p className='hidden xl:block '>
+                                “Drive your business forward — the dashboard is your cockpit.”
+                            </p>
+                            <p className='block xl:hidden'>
+                                "Hey there, boss! Let’s make today’s sales shine."
+                            </p>
+                        </div>
+                    )
+
+            }
+
+        </>
+
     )
 }
 
