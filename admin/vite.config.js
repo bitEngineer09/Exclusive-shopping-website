2aee2bff513eeb1e197e491d8e222fb719bb69aa
bitengineer09 <bitengineer09@gmail.com>
2025-10-25 08:11:09 +0530

    admin vite changes

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [ tailwindcss(),react()],
-})
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  base: '/', // ensure root path, change if subfolder pe host kar rahe ho
+  build: {
+    rollupOptions: {
+      input: '/index.html' // SPA routing ke liye fallback
+    }
+  }
+});
